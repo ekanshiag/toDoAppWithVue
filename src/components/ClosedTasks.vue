9b196a128539e8740a55872eebf9e4c98045b5fa
Ekanshi Agrawal <ekanshi@gmail.com>
2018-08-02 16:11:47 +0530

    Show closed items also as checkbox items and allow to un-do them

--- src/components/ClosedTasks.vue
+++ src/components/ClosedTasks.vue
@@ -1,14 +1,32 @@
 <template>
   <div>
-    {{task.desc}}
+    <input type="checkbox" id="taskItem" :value='task.desc' @click="task.category = 'open'">
+    <label for="taskItem">{{task.desc}}</label>
+    <a @click='showOptions = showOptions ? false : true'>^</a>
+    <options
+    :notes='task.notes'
+    :dueDate='task.dueDate'
+    :priority='task.priority'
+    v-if="showOptions"
+    />
+    <button>Delete</button>
   </div>
 </template>
 
 <script>
+import Options from './Options.vue'
 export default{
   name: 'ClosedTasks',
+  data () {
+    return {
+      showOptions: false
+    }
+  },
   props: {
     task: Object
+  },
+  components: {
+    'options': Options
   }
 }
 </script>
